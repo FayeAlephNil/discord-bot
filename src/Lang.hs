e2d6f3b4f55c7045956967acc550d10b26774335
Faye <justalittleepsilon@gmail.com>
2020-06-11 15:42:16 -0500

    Better Error Handling

--- src/Lang.hs
+++ src/Lang.hs
@@ -3,8 +3,9 @@ module Lang where
 import Prelude hiding (readFile, putStrLn)
 
 import qualified Data.Text.IO as TIO
-import Data.Text (Text, stripPrefix)
+import Data.Text (Text, stripPrefix, pack)
 import System.Process
+import System.Directory (doesFileExist, doesPathExist)
 import qualified Data.Text as T
 
 import Control.Concurrent (threadDelay)
@@ -16,10 +17,18 @@ class (Monad m) => MonadCommand m where
   ls :: String -> m Text
 
 instance MonadCommand IO where
-  readFile = TIO.readFile
+  readFile fp = do
+    b <- doesFileExist fp
+    if b
+      then TIO.readFile fp
+      else pure $ "Error: The file {" <> pack fp <> "} does not exist"
   delay = threadDelay
   putStrLn = TIO.putStrLn
-  ls fp = T.pack <$> readProcess "/bin/ls" (words fp) ""
+  ls fp = do
+    b <- doesPathExist fp
+    if b
+      then T.pack <$> readProcess "/bin/ls" (words fp) ""
+      else pure $ "Error: The path {" <> pack fp <> "} does not exist"
 
 data Expr = GetFlag | Comment Text | Seq [Expr]
   deriving (Show, Eq, Ord)
